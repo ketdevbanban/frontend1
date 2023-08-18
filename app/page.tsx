ce5c814b6a4b024243f7fc72c8da387bf6ea5100
ketdevbanban <125855289+ketdevbanban@users.noreply.github.com>
2023-08-18 13:34:14 +0700

    ket

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex justify-center items-center text-green-500">
+      ketoudone ເກດອຸດອນ ພິມມະສອນ
+    </div>
+  );
+}
